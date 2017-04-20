f82377708250a287c512ae179a184dc8c9ec0da8
Rohit Shrivastava <rohit.shrivastava93@gmail.com>
2017-04-20 16:17:28 +0530

    Updates whole website with new events section and new events page

---
+++ events/webdev/assets/js/custom.js
@@ -0,0 +1,44 @@
+﻿(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+            
+
+            // SLIDER SCRIPTS
+            $('#carousel-slider').carousel({
+                interval: 2000 //TIME IN MILLI SECONDS
+            })
+           
+            //  SCROLL SCRIPT FUNCTION
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+            //scrollReveal scripts
+            window.scrollReveal = new scrollReveal();
+
+            //REVIEWS SPEED
+            $('#reviews').carousel({
+                interval: 4000 //TIME IN MILLI SECONDS
+            })
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
